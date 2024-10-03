91695411ffc7fe97f9ecaf65437fcd6c3b61600b
Matt Wilson <wilsonmatt@susqu.edu>
2024-10-03 12:30:44 -0400

    wk4 graded

--- Week4/Week4_GeneticsAssignment.R
+++ Week4/Week4_GeneticsAssignment.R
@@ -5,6 +5,7 @@
   # Part 2: Despite the drawbacks, give the result or interpretation that you feel most confident in (4 points), and EXPLAIN WHY (6 points).
 #I believe it is a graph of how the presence of Single Nucleotide Polymorphism (SNP's) can effect the adaptation of maritime pine, of which the colors of the dots are representative of these trees at different sites of the same region.
 #I have no idea if any of this is actually correct, It is a very difficult data set to interpret, i just made inferences based of the abstract of the link provided and the exel data indication the different  populations used in the study. All in all 3/10 bad graph :(
+#I agree bad graph - but what can you learn from it, or guess at from it? Maybe there's some geographic feature influencing how/where they could sample regions, i.e. mountains or oceans.
 
 # For your scripting assignment we will use the "ge_data" data frame found in the "stability" package.
   # Install the "stability" package, load it into your R environment, and use the data() function to load the "ge_data". (2 points)
@@ -32,6 +33,7 @@ summary(genotype)
   # Which model is a better fit to explain the yield response, and WHY? (6 points)
 #The yield response would be better explained by the genotype dataset because it has a much larger sample size than the  genotype data frame, and is thus more representative, even though it has a slighlty larger p value, both are well under 0.05 and can thus be considered significant
   # Hint: Does one model seem more likely to be over-fitted?
-
+#Backward! too many non-significant factors in the genotype...environment was a good fit.
 # Which environment would be your very WORST choice for generating a strong yield response? (2 points)
 #within the environment data set, there are two environments that are not marked as being significant data. of those are the intercepts: EnvSargodha, and EnvKhanewal. This is because they do not have the little star next to them in the data fram and are the only ones to produce more positive estimates. 
+#The little star doesn't mean significant...the p-value does.
